a0d3d927395970144032bed41d3e5b2569d3b556
Andrei Jaume <andrei.jaumewillenska@utexas.edu>
2024-06-01 17:49:02 -0500

    Add initial project

---
+++ src/FileFinder.cpp
@@ -0,0 +1,12 @@
+﻿// FileFinder.cpp : Defines the entry point for the application.
+//
+
+#include "FileFinder.h"
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
